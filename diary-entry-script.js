a86051a41e014f72dec71a59200564f45c0f507a
Harshit <168411720+slash-aech@users.noreply.github.com>
2024-11-03 01:58:41 +0530

    why

--- diary-entry-script.js
+++ diary-entry-script.js
@@ -1,9 +1,9 @@
 const DiaryEntryLinks = {
     1: "https://drive.google.com/file/d/1jXmHlKzTsWpSftrWSlA45htgIgWWcme4/view?usp=sharing",
-    2:"https://drive.google.com/file/d/1SXmoNU6VJy1J45X1LbaCAqezADnDQIU2/view?usp=sharing",
-    3:"https://drive.google.com/file/d/19gF-TNxsundyNumWQ88CC28ZmYywInJp/view?usp=sharing"
-    4:"https://drive.google.com/file/d/1x4pksXn5F88E1RgznBRfq5rSWx4Zjt7t/view?usp=sharing",
-    5:"https://drive.google.com/file/d/1P3Sn2rNPElXHz1N_1O2XZ3G3ouxoJvPG/view?usp=sharing",
+    2: "https://drive.google.com/file/d/1SXmoNU6VJy1J45X1LbaCAqezADnDQIU2/view?usp=sharing",
+    3: "https://drive.google.com/file/d/19gF-TNxsundyNumWQ88CC28ZmYywInJp/view?usp=sharing",
+    4: "https://drive.google.com/file/d/1x4pksXn5F88E1RgznBRfq5rSWx4Zjt7t/view?usp=sharing",
+    5: "https://drive.google.com/file/d/1P3Sn2rNPElXHz1N_1O2XZ3G3ouxoJvPG/view?usp=sharing",
 };
 
 let currentDiaryEntry = null;
